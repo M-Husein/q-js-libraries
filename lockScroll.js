c1c372424fe57761cd40f02116a0e311e019fff3
Muhamad Husein <m.husein27@gmail.com>
2020-08-12 01:52:53 +0700

    Add files via upload

---
+++ lockScroll.js
@@ -0,0 +1,17 @@
+/** FROM: https://www.geeksforgeeks.org/how-to-disable-scrolling-temporarily-using-javascript/ */
+export function disableScroll(){ 
+	// Get the current page scroll position 
+	let st = window.pageYOffset || document.documentElement.scrollTop; 
+	let sl = window.pageXOffset || document.documentElement.scrollLeft;
+
+	// if any scroll is attempted, set this to the previous value 
+	window.onscroll = function(e){
+		window.scrollTo(sl, st);
+		e.preventDefault();// OPTION
+	}
+}
+
+export function enableScroll(){ 
+	window.onscroll = noop; 
+}
+
